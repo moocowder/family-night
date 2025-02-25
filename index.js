b32178e789a636d8318f43999326c95cebfac527
maathi <mahdi221b@gmail.com>
2025-02-25 22:56:05 +0100

    fkin cors

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const app = express()
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET")
+  next()
+})
+
 app.get("/manifest.json", (req, res) => {
   const manifest = {
     id: "com.mhdev.family-night",
